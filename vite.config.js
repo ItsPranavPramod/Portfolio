e04f25f03ad256ae5ea442e1c7d1eb7abc345f6d
Pranav P P <pranavdevanivas@gmail.com>
2025-10-05 11:19:45 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/PranavPP_R1B_TechTeam/"
+  base:"/Portfolio/"
 })
